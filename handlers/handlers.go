3dd4cb2264cc26731fcc64946fece4b52eb5fd23
Mike Sander <mike.sander@kroger.com>
2022-11-21 13:26:56 -0500

    Update handlers.go

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,27 +18,33 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 		winners, err := data.ListAllJSON()
 		if err != nil {
 			res.WriteHeader(http.StatusInternalServerError)
-		} else {
-			res.Write(winners)
+			return
 		}
-	} else {
-		filteredWinners, err := data.ListAllByYear(year)
-		if err != nil {
-			res.WriteHeader(http.StatusBadRequest)
-		} else {
-			res.Write(filteredWinners)
-		}
+		res.Write(winners)
 	}
+} else {
+filteredWinners, err := data.ListAllByYear(year)
+if err != nil {
+res.WriteHeader(http.StatusBadRequest)
+return
+}
+res.Write(filteredWinners)
+}
+}
 }
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
 	accessToken := req.Header.Get("X-ACCESS-TOKEN")
 	isTokenValid := data.IsAccessTokenValid(accessToken)
+
 	if !isTokenValid {
 		res.WriteHeader(http.StatusUnauthorized)
 	} else {
-		data.AddNewWinner(req.Body)
+		err := data.AddNewWinner(req.Body)
+		if err != nill {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+		}
 		res.WriteHeader(http.StatusCreated)
 	}
 }
